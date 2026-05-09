0cb6db8b469195769d149dc207c21383e2062fff
raulmay88 <rmay@dexabyte.com.mx>
2026-05-09 14:08:07 -0500

    Helper para scrollear a top al cambiar de menú

--- center-data/src/layouts/MainLayout.tsx
+++ center-data/src/layouts/MainLayout.tsx
@@ -2,10 +2,13 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 import WelcomeModal from "../pages/WelcomeModal";
+import ScrollToTop from "../pages/ScrollToTop";
 
 export default function MainLayout() {
   return (
     <div className="flex flex-col min-h-screen">
+      <ScrollToTop />
+
       <Header />
 
       <WelcomeModal />
---
+++ center-data/src/pages/ScrollToTop.tsx
@@ -0,0 +1,18 @@
+// src/components/common/ScrollToTop.tsx
+
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
+
+  return null;
+}
